b4265063254364a0c40bb091b4e1839315fddbb7
Samuel Z <websamrgt@gmail.com>
2024-02-07 17:05:05 -0300

    backend estudo

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ import { express } from "express";
 import { pkg } from "body-parser";
 import { router } from "./routes/router.js";
 
+import sequelize from "./utils/database.js";
+import association from "./models/Associations.js";
+
 const app = express();
 const { json, urlendcoded } = pkg;
 
 app.use(json());
 app.use(urlendcoded({ extended: true }));
 
-app.listen(300, function () {
-    console.log("Listening from http://localhost:300");
-});
+(async () => {
+  try {
+    association.associations();
+    await sequelize.sync();
+    app.listen(3000, function () {
+      console.log("Server is running on port http://localhost:3000");
+    });
+  } catch (error) {
+    console.log("Erro ao sincronizar o banco de dados", error);
+  }
+})();
 
 app.use("/", router);
